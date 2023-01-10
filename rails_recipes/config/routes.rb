bbf56e314b528c7bc615b916894f73622e059cbe
Bateyjosue <josuebatey19@gmail.com>
2023-01-10 09:38:37 +0200

    Add files setup

---
+++ rails_recipes/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  # devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+end
