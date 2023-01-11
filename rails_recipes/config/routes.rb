be794f9fbc290ca96989d24a3ec26c4f0950b3c4
Bateyjosue <josuebatey19@gmail.com>
2023-01-11 10:55:02 +0200

    UPDATE routes

--- rails_recipes/config/routes.rb
+++ rails_recipes/config/routes.rb
@@ -5,23 +5,21 @@ Rails.application.routes.draw do
   # get 'food/create'
   # get 'food/destroy'
   # get 'home/index'
-  # devise_for :users
-  devise_for :users, path: 'auth',
-                     path_names: {
-                       sign_in: 'login',
-                       sign_out: 'logout',
-                       password: 'secret',
-                       confirmation: 'verification',
-                       unlock: 'unblock',
-                       registration: 'register',
-                       sign_up: 'cmon_let_me_in'
-                     }
+  devise_for :users
+  # devise_for :users, path: 'auth',
+  #                    path_names: {
+  #                      sign_in: 'login',
+  #                      sign_out: 'logout',
+  #                      password: 'secret',
+  #                      confirmation: 'verification',
+  #                      unlock: 'unblock',
+  #                      registration: 'register',
+  #                      sign_up: 'cmon_let_me_in'
+  #                    }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'home#index'
   
-  resources :food, except: [:update]
-  resources :users do
-  end
+  resources :food, only: [:index, :new, :create, :destroy]
 end
